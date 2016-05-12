113ec9597b12a4b54f6b54d1a04f7056ffd7350e
jngaravitoc <jngaravitoc@email.arizona.edu>
2016-05-12 16:33:35 -0700

    3d density

--- octopus/density.py
+++ octopus/density.py
@@ -64,4 +64,24 @@ def grid(X, Y, Z, res):
             rho[i][j] = density(X, Y, Z, mass, [rx[i], ry[j], 0])
     return rho
 
+def grid3d(X, Y, Z, res):
+    """
+    Make a grid with all the densities.
+    :param X, Y, Z:
+         Coordinates of the dots.
+    :param res:
+         Resolution of the square grid.
+    :return rho:
+         A 2d array with the densities.
+    """
+    mass = np.ones(len(X))
+    rho = np.zeros((res, res, res))
+    rx = np.linspace(min(X)+min(X)*0.2, max(X)+max(X)*0.2, res)
+    ry = np.linspace(min(Y)+min(Y)*0.2, max(Y)+max(Y)*0.2, res)
+    rz = np.linspace(min(Z)+min(Z)*0.2, max(Z)+max(Z)*0.2, res)
+    for i in range(res):
+        for j in range(res):
+            for k in range(res):
+                rho[i][j][k] = density(X, Y, Z, mass, [rx[i], ry[j], rz[k])
+    return rho
 
